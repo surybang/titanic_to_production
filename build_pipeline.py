1a342990a1e18f9afd9ada0afd9568fbb5526397
surybang <hos.fabien@outlook.com>
2025-04-21 19:52:52 +0000

    chore: reorganize project structure into modular components
    - main.py: core entry point
    - build_pipeline.py: data processing logic
    - train_evaluate.py: ML model lifecycle

---
+++ build_pipeline.py
@@ -0,0 +1,65 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+
+
+def create_pipeline(
+    n_trees: int,
+    numeric_features: list = ["Age", "Fare"],
+    categorical_features: list = ["Embarked", "Sex"],
+    max_depth: int = None,
+    max_features: str = "sqrt",
+) -> Pipeline:
+    """
+    Create a pipeline for preprocessing and model definition
+
+    Params:
+        n_trees : The number of trees in the random forest
+        numeric_features: The numeric features used in the pipeline
+        categorical_features: The categorical features used in the pipeline
+        max_depth : The maximum depth to the forest. Default is "None"
+        max_features: the maximum number of features used when looking for \
+            the best split. Default is "sqrt"
+
+    Returns:
+        pipe : the Pipeline object
+
+    """
+    # Variables quantitatives
+    numeric_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="median")),
+            ("scaler", MinMaxScaler()),
+        ]
+    )
+
+    # Variables qualitatives
+    categorical_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="most_frequent")),
+            ("onehot", OneHotEncoder()),
+        ]
+    )
+
+    # Preprocessing
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("Preprocessing numerical", numeric_transformer, numeric_features),
+            (
+                "Preprocessing categorical",
+                categorical_transformer,
+                categorical_features,
+            ),
+        ]
+    )
+
+    # Pipeline
+    pipe = Pipeline(
+        [
+            ("preprocessor", preprocessor),
+            ("classifier", RandomForestClassifier(n_estimators=20)),
+        ]
+    )
+    return pipe
---
+++ train_evaluate.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.pipeline import Pipeline
+
+
+def evaluate_model(
+    pipe: Pipeline,
+    X_test: pd.DataFrame,
+    y_test: pd.DataFrame
+) -> tuple:
+    """
+    Evaluate the model by calculating the score and confusion matrix
+
+    Params:
+        pipe: the trained pipeline object
+        X_test: the test data
+        y_test: the true labels from test data
+
+    Returns:
+        tuple: A tuple containing the score and confusion matrix
+    """
+    score = pipe.score(X_test, y_test)
+    matrix = confusion_matrix(y_test, pipe.predict(X_test))
+    return score, matrix
