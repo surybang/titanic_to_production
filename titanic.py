1fd5e3da1d7123790cd90d03a68c1455bf114a61
surybang <hos.fabien@outlook.com>
2025-04-21 19:59:44 +0000

    chore: moved files according to cookiecutter

---
+++ titanic.py
@@ -0,0 +1,58 @@
+"""
+Prédiction de la survie d'un individu sur le titanic
+"""
+
+import os
+import argparse
+from dotenv import load_dotenv
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from pipeline.build_pipeline import create_pipeline
+from models.train_evaluate import evaluate_model
+
+# ENVIRONNEMENT CONFIG ---------------------------
+load_dotenv()
+
+parser = argparse.ArgumentParser(description="Combien d'arbres ?")
+parser.add_argument(
+    "--n_trees", type=int, default=20, help="Nombre d'arbres à utiliser"
+)
+args = parser.parse_args()
+print(f"Nombre d'arbres utilisés pour la classification : {args.n_trees}")
+
+n_trees = args.n_trees
+MAX_DEPTH = None
+MAX_FEATURES = "sqrt"
+
+jeton_api = os.environ.get("JETON_API", "")
+data_path = os.environ.get("DATA_PATH")
+data_train_path = os.environ.get("train_path", "data/derived/train.csv")
+data_test_path = os.environ.get("test_path", "data/derived/test.csv")
+
+if jeton_api.startswith("$"):
+    print("API token has been configured properly")
+else:
+    print("API token has not been configured")
+
+# IMPORT DATA --------------------------
+TrainingData = pd.read_csv(data_path)
+
+y = TrainingData["Survived"]
+X = TrainingData.drop("Survived", axis="columns")
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+pd.concat([X_train, y_train], axis=1).to_csv("train.csv")
+pd.concat([X_test, y_test], axis=1).to_csv("test.csv")
+
+# PIPELINE ----------------------------
+pipe = create_pipeline(n_trees)
+
+# RUNNING
+pipe.fit(X_train, y_train)
+score, matrix = evaluate_model(pipe, X_test, y_test)
+print(f"{score:.1%} de bonnes réponses sur les données de test pour validation")
+print(20 * "-")
+print("matrice de confusion")
+print(matrix)
