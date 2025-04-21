71b3077fd66b19185f8f61b95540adbead3f7bb9
surybang <hos.fabien@outlook.com>
2025-04-21 18:01:32 +0000

    chore: restructured chunks for evaluate and import data

--- titanic.py
+++ titanic.py
@@ -33,39 +33,14 @@ MAX_DEPTH = None
 MAX_FEATURES = "sqrt"
 
 jeton_api = os.environ.get("JETON_API", "")
+data_path = os.environ.get("DATA_PATH")
+
 if jeton_api.startswith("$"):
     print("API token has been configured properly")
 else:
     print("API token has not been configured")
 
-# Import data
-os.chdir("/home/onyxia/work/titanic_to_production")
-TrainingData = pd.read_csv("data.csv")
-TrainingData.head()
-TrainingData["Ticket"].str.split("/").str.len()
-TrainingData["Name"].str.split(",").str.len()
-TrainingData.isnull().sum()
-
-## Un peu d'exploration et de feature engineering
-### Statut socioéconomique
-fig, axes = plt.subplots(
-    1, 2, figsize=(12, 6)
-)  # layout matplotlib 1 ligne 2 colonnes taile 16*8
-fig1_pclass = sns.countplot(data=TrainingData, x="Pclass", ax=axes[0]).set_title(
-    "fréquence des Pclass"
-)
-fig2_pclass = sns.barplot(
-    data=TrainingData, x="Pclass", y="Survived", ax=axes[1]
-).set_title("survie des Pclass")
-
-
-### Age
-sns.histplot(data=TrainingData, x="Age", bins=15, kde=False).set_title(
-    "Distribution de l'âge"
-)
-plt.show()
-
-## Encoder les données imputées ou transformées.
+# FUNCTIONS -------------------------------------
 def create_pipeline(
     n_trees: int,
     numeric_features: list = ["Age", "Fare"],
@@ -146,28 +121,24 @@ def evaluate_model(
     matrix = confusion_matrix(y_test, pipe.predict(X_test))
     return score, matrix
 
-# splitting samples
+
+# IMPORT DATA --------------------------
+TrainingData = pd.read_csv(data_path)
+
 y = TrainingData["Survived"]
 X = TrainingData.drop("Survived", axis="columns")
 
-# On _split_ notre _dataset_ d'apprentisage pour faire de la validation croisée \
-# une partie pour apprendre une partie pour regarder le score.
-# Prenons arbitrairement 10% du dataset en test et 90% pour l'apprentissage.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 pd.concat([X_train, y_train], axis=1).to_csv("train.csv")
 pd.concat([X_test, y_test], axis=1).to_csv("test.csv")
 
-# Random Forest
-# Ici demandons d'avoir 20 arbres
+# PIPELINE ----------------------------
+pipe = create_pipeline(n_trees)
+
+# RUNNING
 pipe.fit(X_train, y_train)
-
-
-# calculons le score sur le dataset d'apprentissage et sur le dataset de test \
-# (10% du dataset d'apprentissage mis de côté)
-# le score étant le nombre de bonne prédiction
-rdmf_score = pipe.score(X_test, y_test)
-rdmf_score_tr = pipe.score(X_train, y_train)
-print(f"{rdmf_score:.1%} de bonnes réponses sur les données de test pour validation")
+score, matrix = evaluate_model(pipe, X_test, y_test)
+print(f"{score:.1%} de bonnes réponses sur les données de test pour validation")
 print(20 * "-")
 print("matrice de confusion")
-print(confusion_matrix(y_test, pipe.predict(X_test)))
+print(matrix)
