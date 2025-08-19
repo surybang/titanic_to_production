6d6146426abf33940121749a97310897a7f3f024
surybang <hos.fabien@outlook.com>
2025-08-19 12:54:37 +0000

    test: tutorial script airflow 2.10.3

--- dags/tutorial.py
+++ dags/tutorial.py
@@ -1,17 +1,20 @@
 import textwrap
 from datetime import datetime, timedelta
 
-# Operators; we need this to operate!
-from airflow.providers.standard.operators.bash import BashOperator
-
 # The DAG object; we'll need this to instantiate a DAG
-from airflow.sdk import DAG
+from airflow.models.dag import DAG
+
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
 with DAG(
     "tutorial",
     # These args will get passed on to each operator
     # You can override them on a per-task basis during operator initialization
     default_args={
         "depends_on_past": False,
+        "email": ["airflow@example.com"],
+        "email_on_failure": False,
+        "email_on_retry": False,
         "retries": 1,
         "retry_delay": timedelta(minutes=5),
         # 'queue': 'bash_queue',
@@ -19,6 +22,7 @@ with DAG(
         # 'priority_weight': 10,
         # 'end_date': datetime(2016, 1, 1),
         # 'wait_for_downstream': False,
+        # 'sla': timedelta(hours=2),
         # 'execution_timeout': timedelta(seconds=300),
         # 'on_failure_callback': some_function, # or list of functions
         # 'on_success_callback': some_other_function, # or list of functions
